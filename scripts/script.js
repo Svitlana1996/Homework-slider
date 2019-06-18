d99a2d2dddf6422bdcfc677d057e3c8d6612c2dd
Svitlana1996 <lanaovsiichuk@gmail.com>
2019-06-18 15:11:59 +0300

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        prevArrow: '.arrow--1',
+        nextArrow: '.arrow--2',
+                         
+    })
+})
